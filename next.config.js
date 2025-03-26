ad1c6cec7f97ed6b46f953ff79109e45c2815a53
Anirudhmadhavkulkarni9094 <Anirudhkulkarni9094@gmail.com>
2025-03-26 22:58:15 +0530

    adding all route for bypass cors

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const nextConfig = {
   images: {
     domains: ["picsum.photos"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Allow all origins (NOT recommended for production)
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
